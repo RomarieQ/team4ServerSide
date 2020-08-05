a08ca0a84bca74c7754449add300171ffdf3d978
travisjslagle <travisjslagle@gmail.com>
2020-08-05 14:16:55 -0400

    initial server commit

---
+++ app.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+let express = require('express');
+let app = express();
+let sequelize = require('./db');
+
+// CONTROLLERS GO HERE
+// let user = require(userController)
+// let location = require(locationController)
+
+sequelize.sync();
+
+// app.use(require('./middleware/headers'));
+
+app.use(express.json());
+
+// EXPOSED ROUTE -- for user endpoints
+app.use('/user', user);
+
+
+// PROTECTED ROUTE -- for location enpoints
+//app.use(require('./middleware/validate-session'));  // can un-comment this out once validateSession is implemented, all routes below it will require a token
+app.use('/location', location);
+
+app.listen(3000, function(){
+    console.log('Travel App is listening on port 3000 -- pack your bags!');
+})
---
+++ db.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('travel-app', 'postgres', process.env.DB_PASS, {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+
+sequelize.authenticate().then(
+    function() {
+        console.log('connected to travel-app postgress database');
+    },
+    function(err) {
+        console.log(err);
+    }
+);
+module.exports = sequelize;

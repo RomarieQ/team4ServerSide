f404f9603958207b06b864a27ce174ed9dab262f
Romarie E. Quinones <romiquin86@romaries-air.attlocal.net>
2020-08-06 17:12:56 -0400

    validate session

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let express = require('express');
 let app = express();
 let sequelize = require('./db');
 
+let user = require('./controllers/usercontroller');
+
 // CONTROLLERS GO HERE
 // let user = require(userController)
 // let location = require(locationController)
@@ -20,6 +22,7 @@ app.use('/user', user);
 // PROTECTED ROUTE -- for location enpoints
 //app.use(require('./middleware/validate-session'));  // can un-comment this out once validateSession is implemented, all routes below it will require a token
 app.use('/location', location);
+app.use(require('./middleware/validate-session'));
 
 app.listen(3000, function(){
     console.log('Travel App is listening on port 3000 -- pack your bags!');
--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../db').import('../models/user');
+const jwt = require('jsonwebtoken');
 
 
 //User signup
@@ -10,8 +11,13 @@ router.post('/create', function(req, res) {
     })
     .then(
         function createSuccess(user) {
+
+            let token = jwt.sign({id: user.id, email: user.email}, "process.env.JWT_SECRET", {expiresIn: 60*60*24});
+
             res.json({
-                user: user
+                user: user,
+                message: 'User successfully created',
+                sessionToken: token
             });
         }
     )
@@ -28,8 +34,12 @@ router.post('/login', function(req, res) {
     })
     .then(function loginSuccess(user) {
         if (user) {
+            let token = jwt.sign({id: user.id}, "process.env.JWT_SECRET", {expiresIn: 60*60*24})
+
         res.status(200).json({
-            user: user
+            user: user,
+            message: 'User successfuly logged in',
+            sessionToken: token
         })
     } else {
         res.status(500).json({error: 'User does not exist'})

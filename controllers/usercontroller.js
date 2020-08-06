4eb9a089f6304511fe1828fe6613f47c0e469271
Romarie E. Quinones <romiquin86@romaries-air.attlocal.net>
2020-08-06 15:47:18 -0400

    fixed

---
+++ controllers/usercontroller.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const User = require('../db').import('../models/user');
+
+
+//User signup
+router.post('/create', function(req, res) {
+    User.create({
+        email: req.body.user.email,
+        password: req.body.user.password
+    })
+    .then(
+        function createSuccess(user) {
+            res.json({
+                user: user
+            });
+        }
+    )
+    .catch(err => res.status(500).json({erro: err}))
+});
+
+
+//login
+router.post('/login', function(req, res) {
+    User.findOne({
+        where: {
+            email: req.body.user.email
+        }
+    })
+    .then(function loginSuccess(user) {
+        if (user) {
+        res.status(200).json({
+            user: user
+        })
+    } else {
+        res.status(500).json({error: 'User does not exist'})
+    }
+    })
+    .catch(err => res.status(500).json({error: err}))
+});
+
+module.exports = router;

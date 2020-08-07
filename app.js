1776e4f00c68f8d33c775518f5fa0b43b9926ff3
travisjslagle <travisjslagle@gmail.com>
2020-08-07 10:12:28 -0400

    all endpoints working!

--- app.js
+++ app.js
@@ -3,15 +3,13 @@ let express = require('express');
 let app = express();
 let sequelize = require('./db');
 
-let user = require('./controllers/usercontroller');
-
 // CONTROLLERS GO HERE
 let user = require('./controllers/usercontroller');
 let location = require('./controllers/locationcontroller');
 
 sequelize.sync();
 
-// app.use(require('./middleware/headers'));
+app.use(require('./middleware/headers'));
 
 app.use(express.json());
 
@@ -20,9 +18,8 @@ app.use('/user', user);
 
 
 // PROTECTED ROUTE -- for location enpoints
-//app.use(require('./middleware/validate-session'));  // can un-comment this out once validateSession is implemented, all routes below it will require a token
-app.use('/location', location);
 app.use(require('./middleware/validate-session'));
+app.use('/location', location);
 
 app.listen(3000, function(){
     console.log('Travel App is listening on port 3000 -- pack your bags!');
--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,25 +1,24 @@
 const router = require('express').Router();
 const User = require('../db').import('../models/user');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 
 //User signup
 router.post('/create', function(req, res) {
     User.create({
         email: req.body.user.email,
-        password: req.body.user.password
+        password: bcrypt.hashSync(req.body.user.password, 13)
     })
     .then(
         function createSuccess(user) {
 
-            let token = jwt.sign({id: user.id, email: user.email}, "process.env.JWT_SECRET", {expiresIn: 60*60*24});
+            let token = jwt.sign({id: user.id, email: user.email}, process.env.JWT_SECRET, {expiresIn: 60*60*24});
 
             res.json({
                 user: user,
                 message: 'User successfully created',
                 sessionToken: token
-
-
             });
         }
     )
@@ -36,12 +35,19 @@ router.post('/login', function(req, res) {
     })
     .then(function loginSuccess(user) {
         if (user) {
-            let token = jwt.sign({id: user.id}, "process.env.JWT_SECRET", {expiresIn: 60*60*24})
 
-        res.status(200).json({
-            user: user,
-            message: 'User successfuly logged in',
-            sessionToken: token
+        bcrypt.compare(req.body.user.password, user.password, function(err, matches) {
+            if(matches){
+                let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60*60*24})
+    
+            res.status(200).json({
+                user: user,
+                message: 'User successfuly logged in',
+                sessionToken: token
+            })
+        } else {
+            res.status(500).json({ error: 'Email or Password is Incorrect'})
+            }
         })
     } else {
         res.status(500).json({error: 'User does not exist'})

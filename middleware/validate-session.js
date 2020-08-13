db3c1970bcdfb29d8504f59a2bc2b6a198851105
Romarie E. Quinones <romiquin86@romaries-air.attlocal.net>
2020-08-13 17:06:00 -0400

    validate

--- middleware/validate-session.js
+++ middleware/validate-session.js
@@ -3,9 +3,11 @@ const User = require('../db').import('../models/user');
 
 const validateSession = (req, res, next) => {
     const token = req.headers.authorization;
-
-    if (!token) {
-        return res.status(403).send({ auth: false, message: "No token provided"})
+    if (req.method === "OPTIONS") {
+        next();
+    } else {
+        if (!token) {
+            return res.status(403).send({ auth: false, message: "No token provided"})
     } else {
         jwt.verify(token, process.env.JWT_SECRET, (err, decodeToken) => {
             if (!err && decodeToken) {
@@ -26,6 +28,9 @@ const validateSession = (req, res, next) => {
                 return res.status(500).send('Not Authorized');
             }
         });
+
+    }
+    
     }
 };
 

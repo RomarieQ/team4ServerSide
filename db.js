d5987203cc121d9ce30c19138997d1a0b5a62f85
Romarie E. Quinones <romiquin86@romaries-air.attlocal.net>
2020-08-13 11:28:54 -0400

    heroku changes

--- db.js
+++ db.js
@@ -2,6 +2,10 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres'
 });
+// const sequelize = new Sequelize('travel-app', 'postgres', process.env.DB_PASS, {
+//     host: 'localhost',
+//     dialect: 'postgres'
+// });
 
 sequelize.authenticate().then(
     function() {
